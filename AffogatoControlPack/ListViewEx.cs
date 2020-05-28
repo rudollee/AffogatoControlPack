1964b1d28d8cfcf26e7afc09a89f0b257d0216c9
wook@DESKTOP04 <rudollee@gmail.com>
2020-05-28 16:18:47 +0900

    Add project files.

---
+++ AffogatoControlPack/ListViewEx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace AffogatoControlPack
+{
+    public class ListViewEx : ListView
+    {
+        [DllImport("uxtheme", CharSet = CharSet.Auto)]
+        static extern Boolean SetWindowTheme(IntPtr hWindow, String subAppName, String subIDList);
+
+        public ListViewEx()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.ResizeRedraw, true);
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            SetWindowTheme(Handle, "explorer", null);
+        }
+    }
+}
